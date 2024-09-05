42897f0f45ab2448273e621b9bd4e639319076e5
Bethany Fannin <contact@bethanyfannin.com>
2024-09-05 14:49:01 -0400

    feat(commands): add robots command
    BREAKING CHANGE: Add robots command that runs by default

--- src/commands/robots.ts
+++ src/commands/robots.ts
@@ -1,11 +1,7 @@
 // src/commands/robots.ts
-import {Command, Flags, Args} from '@oclif/core'
+import {Args, Command, Flags} from '@oclif/core'
 
 export default class Robots extends Command {
-  static description = 'Generate robots.txt file'
-
-  static examples = ['<%= config.bin %> <%= command.id %>  --disallow / --sitemap ./sitemap.xml --user-agent default']
-
   static override args = {
     output: Args.string({
       description: 'Output file path',
@@ -13,27 +9,31 @@ export default class Robots extends Command {
     }),
   }
 
+  static description = 'Generate robots.txt file'
+
+  static examples = ['<%= config.bin %> <%= command.id %>  --disallow / --sitemap ./sitemap.xml --user-agent default']
+
   static override flags = {
+    block: Flags.string({
+      char: 'b',
+      default: 'default',
+      description: 'User-agents to block',
+      multiple: true,
+      required: false,
+    }),
     path: Flags.string({
       char: 'p',
-      description: 'Paths to block the user agents from crawling',
-      required: false,
-      multiple: true,
       default: './',
+      description: 'Paths to block the user agents from crawling',
       helpValue: ['./', './admin', './wp-admin'],
+      multiple: true,
+      required: false,
     }),
     sitemap: Flags.boolean({
       char: 's',
+      default: false,
       description: 'Whether to include a sitemap in the robots.txt file',
       required: false,
-      default: false,
-    }),
-    block: Flags.string({
-      char: 'b',
-      description: 'User-agents to block',
-      required: false,
-      multiple: true,
-      default: 'default',
     }),
   }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
 import {assert} from 'chai'
 import {describe, it} from 'mocha'
-import run from '../src/index'
+
+import run from '../src/index.js'
 
 describe('run', () => {
   it('should return hello world', () => {
